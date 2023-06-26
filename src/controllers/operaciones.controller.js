64b34970e8c33d5cd0c813d22555680540e531dc
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-06-26 08:54:26 -0500

    feat: arquitectura de 3 capas

--- src/controllers/operaciones.controller.js
+++ src/controllers/operaciones.controller.js
@@ -1,5 +1,13 @@
+import { restar } from '../services/operaciones.service.js'
+
 export const suma = (req, res) => {
     const num1 = parseInt(req.query.num1)
     const num2 = parseInt(req.query.num2)
     res.json({ status: 'success', payload: num1 + num2 })
+}
+
+export const resta = (req, res) => {
+    const { num1, num2 } = req.query
+    const result = restar(num1, num2)
+    res.json({ status: 'success', payload: result })
 }
--- src/routers/operaciones.router.js
+++ src/routers/operaciones.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express"
-import { suma } from '../controllers/operaciones.controller.js'
+import { suma, resta } from '../controllers/operaciones.controller.js'
 
 const router = Router()
 
 router.get('/suma', suma)
+router.get('/resta', resta)
 
 export default router
