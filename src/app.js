4fcd6ef5f11d576af19c8193b1927710bb2b0f3a
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-06-26 08:43:47 -0500

    feat: delete routers

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 import express from 'express'
 
 const app = express()
+app.get('/api/operaciones', (req, res) => res.send('ok!'))
+app.get('/api/operaciones/suma', (req, res) => {
+    const num1 = parseInt(req.query.num1)
+    const num2 = parseInt(req.query.num2)
+    res.json({ status: 'success', payload: num1 + num2 })
+})
 
 app.listen(8080, () => console.log('Server Up!'))
